c201fb2cd9e35cf83731a20bb701d616e324faf6
sturnergeddon <51706289+sturnergeddon@users.noreply.github.com>
2021-05-10 16:35:17 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="skeleton-container">
+      <div className="skeleton-circle" />
+      <div className="skeleton-rect" />
+      <div className="skeleton-rect" />
+
+      <div className="skeleton-rect" />
+      <div className="skeleton-rect" />
+    </div>
+  );
+}
